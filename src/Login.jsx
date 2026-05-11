f7baae852d34fb76336e4d145f10d725b8451f0c
Rachana <rachanaacharya186@gmail.com>
2026-05-11 17:55:24 +0530

    Fixed frontend backend URLs

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,13 +5,16 @@ function Login() {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:8082/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    const res = await fetch(
+      "https://jobtrackerback-production.up.railway.app/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      }
+    );
 
     const msg = await res.text();
     alert(msg);
